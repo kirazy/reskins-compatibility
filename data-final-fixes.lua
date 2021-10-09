f2b9d664378b8a98d020d3136145ac9d7325dddd
Kira <35714562+kirazy@users.noreply.github.com>
2021-10-09 01:03:39 -0700

    Add spaceblock support

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -11,6 +11,7 @@ require("prototypes.mods.mini-machines") -- Mini-machines sets up machines in da
 require("prototypes.mods.deadlock-crating") -- DeadlockCrating sets up machines in data-final-fixes
 require("prototypes.mods.deadlock-stacking-addons.science-packs") -- Stack items and recipes are generated in data-final-fixes
 require("prototypes.mods.angels-smelting-extended.angels-smelting-extended-items")
+require("prototypes.mods.spaceblock") -- Spaceblock matter furnace is setup in data-final-fixes
 
 -- Assign deferred icons
 reskins.lib.assign_deferred_icons("compatibility", "data-final-fixes")
