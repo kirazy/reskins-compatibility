35eb1d99f4e34c115a1aa370cf54eb5ac9789b72
Kira <35714562+kirazy@users.noreply.github.com>
2022-12-30 16:09:01 -0800

    Add support for nuclear reactor technologies

--- prototypes/mods/realisticreactorglow/technology-updates.lua
+++ prototypes/mods/realisticreactorglow/technology-updates.lua
@@ -18,16 +18,29 @@ local inputs = {
     icon_name = "nuclear-power",
 }
 
-
 local light_color = ""
 if reskins.lib.setting("RealisticReactorGlow-cyan") then
     light_color = "_cyan"
 end
 
+local material_tiers = {
+    "base",
+    "silver-aluminum",
+    "gold-copper",
+}
+
+if reskins.lib.migration.is_version_or_newer(mods["bobpower"], "1.1.6") then
+    material_tiers = {
+        "aluminum-invar",
+        "silver-titanium",
+        "gold-copper",
+    }
+end
+
 local technologies = {
-    ["nuclear-power"] = {tier = 1, prog_tier = 3, icon_base = "nuclear-power-1-color"..light_color, tint = reskins.bobs.nuclear_reactor_index["nuclear-reactor"].tint}, -- t3 reactor
-    ["bob-nuclear-power-2"] = {tier = 2, prog_tier = 4, icon_base = "nuclear-power-2-color"..light_color, tint = reskins.bobs.nuclear_reactor_index["nuclear-reactor-2"].tint}, -- t4 reactor
-    ["bob-nuclear-power-3"] = {tier = 3, prog_tier = 5, icon_base = "nuclear-power-3-color"..light_color, tint = reskins.bobs.nuclear_reactor_index["nuclear-reactor-3"].tint}, -- t5 reactor
+    ["nuclear-power"] = {tier = 1, prog_tier = 3, icon_base = "nuclear-power-"..material_tiers[1].."-color"..light_color, tint = reskins.bobs.nuclear_reactor_index["nuclear-reactor"].tint}, -- t3 reactor
+    ["bob-nuclear-power-2"] = {tier = 2, prog_tier = 4, icon_base = "nuclear-power-"..material_tiers[2].."-color"..light_color, tint = reskins.bobs.nuclear_reactor_index["nuclear-reactor-2"].tint}, -- t4 reactor
+    ["bob-nuclear-power-3"] = {tier = 3, prog_tier = 5, icon_base = "nuclear-power-"..material_tiers[3].."-color"..light_color, tint = reskins.bobs.nuclear_reactor_index["nuclear-reactor-3"].tint}, -- t5 reactor
 }
 
 reskins.lib.create_icons_from_list(technologies, inputs)
